a0f3223331e5dd4063fe4e68a36becbfa5790ccb
Thomas Dewitte <thomasdewittecontact@gmail.com>
2024-06-02 23:06:12 +0200

    Added user role for restricting access

--- includes/session.php
+++ includes/session.php
@@ -1,8 +1,19 @@
 <?php
+    function getRole($id) {
+        require "connect.php";
+
+        $sql = "SELECT role FROM USERS WHERE user_id = $id";
+        $result = mysqli_query($conn, $sql);
+        $row = mysqli_fetch_assoc($result);
+
+        mysqli_close($conn);
+        return $row['role'];
+    }
+
     session_start();
 
     if (isset($_SESSION["user_id"])) {
-        $user_id = $_SESSION["user_id"];
+        $_SESSION["user_role"] = getRole($_SESSION["user_id"]);
     } else {
         header("Location: login.php");
         exit;
