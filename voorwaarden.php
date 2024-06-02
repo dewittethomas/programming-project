3eb502579a338fad2315db3008deb205f3f353eb
RobbeWls <robbewillemyns@gmail.com>
2024-06-02 14:09:25 +0200

    Update voorwaarden.php

--- voorwaarden.php
+++ voorwaarden.php
@@ -62,30 +62,27 @@
 
     <main class="container-voorwaarde">
         <p class="titel-voorwaarde">1. Algemene Informatie</p><br>
-        <p>Een ontmoetingsplaats voor creativiteit en technologie in Brussel. Een open maakplaats waar jong en oud de handen uit de mouwen kunnen steken,
-             technologieën kunnen verkennen en kunnen deelnemen aan begeleide workshops.
-
-        <p>Bij ons is iedereen welkom, of je nu een leraar bent die met zijn klas wil bijleren, een student met een creatief concept of een beginnende ondernemer
-             op zoek naar ruimte om te experimenteren. Iedereen die ideeën en concepten wilt omzetten naar tastbare producten is hier thuis.</p>
+        <p>De uitleendienst van het medialab is een dienst die studenten en leerkrachten van de EhB de mogelijkheid biedt om materiaal te lenen
+             voor hun projecten. Dit materiaal kan variëren van camera's tot belichting en audio apparatuur.</p>
         <br>
         <p class="titel-voorwaarde">2. Toegangsvoorwaarden</p><br>
-        <p><b>Extern:</b> Stuur een mailtje naar medialab@ehb.be of vul het <a href="https://www.erasmushogeschool.be/nl/onderzoek/labs/medialab.brussels/tarieven">aanvraagformulier</a> in.</p>
-        <p> Geldende tarieven kan je <a href="https://www.erasmushogeschool.be/nl/onderzoek/labs/medialab.brussels/tarieven">hier</a> terugvinden. </p>
-        <p> Ben je <b>student of leerkracht</b> aan de EhB? Reserveer je spot via onze online uitleendienst.</p>
+        <p><b>Extern:</b> Stuur een mailtje naar medialab@ehb.be.</p>
+        <p> Ben je <b>student of leerkracht</b> aan de EhB? Reserveer je materiaal via onze online uitleendienst.</p>
         <br>
         <p class="titel-voorwaarde">3. Uitleenvoorwaarden</p><br>
-        <p>Bij het uitlenen van apperatuur wordt verwacht dat de gebruiker zijn <b>studentenkaart/confirmatiemail</b> kan voorstellen aan de ingang van het medialab</p>
+        <p>Bij het uitlenen van apperatuur wordt verwacht dat de lener een <b>bewijs van identiteit</b> kan voorstellen aan de medewerker van het medialab</p>
         <p>De duur van het uitlenen staat vast op een <b>maximum van 1 week</b>. Deze kan verlengd worden naar 2 weken. Niet opdagen voor een ophaling van het materiaal betekent dat de
-            uitlening geannuleerd wordt en je deze opnieuw moet uitlenen. </p>
+            uitlening geannuleerd wordt en je dat je dit materiaal opnieuw moet reserveren. Wanneer je een product te laat terugbrengt krijg je een <b>waarschuwing</b>. Bij 2 waarschuwingen
+            wordt je op de zwarte lijst gezet en zal het niet meer mogelijk zijn materiaal te lenen. </p>
         <br>
         <p class="titel-voorwaarde">4. Richtlijnen</p><br>
         <p>Het materiaal moet met <b>respect</b> behandeld worden. Bij indiening van het materiaal wordt het <b>gecontroleerd op schade</b>. Moest er een vorm van schade worden gevonden, word de
             gebruiker hiervoor verantwoordelijk gehouden en wordt er een <b>vorm van compensatie</b> verwacht.
         </p>
-        <p>Bij het 2 keer voorkomen van schade aan materiaal wordt de gebruiker op de blacklist geplaatst en is deze ongeschikt om nog verder materiaal uit te lenen.</p>
+        <p>Bij het 2 keer voorkomen van schade aan materiaal wordt de gebruiker op de zwarte lijst geplaatst en is deze ongeschikt om nog verder materiaal uit te lenen.</p>
         <br>
         <p class="titel-voorwaarde">5. Diefstal</p><br>
-        <p>Bij diefstal moet u de school direct verwittigen en wordt automatisch de hulpdiensten geïnformeerd.</p>
+        <p>Bij diefstal moet u de school direct verwittigen en worden meteen de hulpdiensten geïnformeerd.</p>
     </main>
     
     <footer>
