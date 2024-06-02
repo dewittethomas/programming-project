ac4291791688627b9e7b3fc7a45a1ef4d96daf4c
Thomas Dewitte <thomasdewittecontact@gmail.com>
2024-06-02 23:18:32 +0200

    Updated admin navigation

--- scansysteem.php
+++ scansysteem.php
@@ -1,3 +1,11 @@
+<?php
+    require "includes/session.php";
+
+    if ($_SESSION["user_role"] != "admin") {
+        header("Location: index.php");
+        exit;
+    }
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -28,12 +36,12 @@
     <div class="header-bottom">
         <div class="container">
             <ul class="category-container">
-                <li><a href="scansysteem.php">Scanning</a></li>
-                <li><a href="artikel-toevoegen.php">Artikel toevoegen</a></li>
-                <li><a href ="blacklist.php">Blacklist</a></li>
-                <li><a href ="admin-producten.php">Producten</a></li>
-                <li><a href ="waarschuwen.php">Waarschuwen</a></li>
-
+                    <li><a href="admin.php">Agenda</a></li>
+                    <li><a href="scansysteem.php">Scansysteem</a></li>
+                    <li><a href="admin-producten.php">Producten</a></li>
+                    <li><a href="artikel-toevoegen.php">Artikel toevoegen</a></li>
+                    <li><a href="blacklist.php">Blacklist</a></li>
+                    <li><a href="waarschuwen.php">Waarschuwen</a></li>
             </ul>
         </div>
     </div>
